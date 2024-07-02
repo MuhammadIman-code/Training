0a60f7810fe4966bb5003d053088271f72fd970c
MUHAMMAD IMAN <muhammadimanirfan219@gmail.com>
2024-07-02 00:37:15 -0700

    .env added

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app =express();
 const con = require('./db')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+require('dotenv').config()
+const port = process.env.Port
 
 
 const medRouter = require('./routes/medicine')
 //iman imangit
+
  app.use('/medicines',medRouter)
-app.listen(3000)
+app.listen(port)
