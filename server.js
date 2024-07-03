f9cd8133a1d26af2e9133276d7c7af7acaa0b5f3
MUHAMMAD IMAN <muhammadimanirfan219@gmail.com>
2024-07-03 11:44:02 -0700

    v3.0

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const con = require('./db')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 require('dotenv').config()
-const port = process.env.Port
+const port = process.env.Port || 3000
+
+const log = (req,res,next)=>{
+    console.log(`${new Date().toLocaleString()} Request Made to ${req.originalUrl}`);
+    next();
+}
+app.use(log)
 
 
 const medRouter = require('./routes/medicine')
